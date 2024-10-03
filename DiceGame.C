ae955a5fccf659d7deffcc8e69d71414cd47342b
Cesar Miranda <cesarmirandacx602@gmail.com>
2024-10-03 08:47:18 -0700

    added randomness to dice & template code for game

--- DiceGame.C
+++ DiceGame.C
@@ -1,10 +1,18 @@
 #include <stdio.h>
+#include <time.h> // Used for Random Number
+#include <stdlib.h> // Used for Random Number
 
 // Function Declerations (prototypes)
-void randomNUmberDice(int *die1,int *die2);
+void randomNumberDice(int *dice1,int *dice2);
+void startGame(char playerOneName[], char playerTwoName[]);
 
 int main() {
-    char playerOneName[50];  // Allocate enough space for the name
+    srand(time(NULL)); // Used to intialze the random number. 
+
+    // initializing variables
+    int playerOneScore = 0;
+    int playerTwoScore = 0;
+    char playerOneName[50];  // Allocatign enough space for the name
     char playerTwoName[50]; 
 
     // Prompt for the player One's name
@@ -19,8 +27,15 @@ int main() {
     printf("Player 1's name is: %s\n", playerOneName);
     printf("Player 2's name is: %s\n", playerTwoName);
 
+    startGame(playerOneName, playerTwoName);
+
     return 0;
 }
 
+void startGame(char playerOneName[], char playerTwoName[]){
 
-
+}
+void randomNumberDice(int *dice1, int *dice2) {
+    *dice1 = rand() % 6 + 1;  // Generate a random number between 1 and 6
+    *dice2 = rand() % 6 + 1;
+}
